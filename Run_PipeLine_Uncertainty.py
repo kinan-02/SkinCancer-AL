74a74d59aaef7898a1ae3a42604936f4136d0389
kinan-02 <kinan.i.2002@gmail.com>
2024-10-29 15:50:00 +0200

    Diversity PipeLine

--- Run_PipeLine_Uncertainty.py
+++ Run_PipeLine_Uncertainty.py
@@ -49,7 +49,7 @@ def main():
                                           train_df=train_df,
                                           appraoch='Uncertainty')
         accuracy_scores_dict[criterion] = AL_class.run_pipeline()
-    with open('uncertainty_approaches_accuracy.pkl', 'wb') as file:
+    with open('competence_accuracy.pkl', 'wb') as file: #uncertainty_approaches
         # Write the list to the file using pickle
         pickle.dump(accuracy_scores_dict, file)
 if __name__ == "__main__":
